c6edf14bdeecacae562f431ebd151b757678f51e
sysumeepo <sysumeepo@gmail.com>
2015-09-14 09:30:35 +0800

    debug

--- backend/callback.php
+++ backend/callback.php
@@ -6,23 +6,19 @@
  * Time: 上午11:40
  */
 
-//require_once "lib/WxPay.Config.php";
-//require_once "lib/wechat.class.php";
+require_once "lib/WxPay.Config.php";
+require_once "lib/wechat.class.php";
 
-if (isset($_GET['code'])) {
-    echo $_GET['code'];
-} else {
-    echo "NO CODE";
-}
 
-//$opt = array(
-//    'appid' => WxPayConfig::APPID,    //填写高级调用功能的appid
-//    'appsecret' => WxPayConfig::APPSECRET //填写高级调用功能的密钥
-//);
+$opt = array(
+    'appid' => WxPayConfig::APPID,    //填写高级调用功能的appid
+    'appsecret' => WxPayConfig::APPSECRET //填写高级调用功能的密钥
+);
 
-//$we = new Wechat($opt);
-//if ($accessRtn = $we->getOauthAccessToken()) {
-//    if ($userInfo = $we->getOauthUserinfo($accessRtn['access_token'], $accessRtn['openid'])) {
+$we = new Wechat($opt);
+if ($accessRtn = $we->getOauthAccessToken()) {
+    if ($userInfo = $we->getOauthUserinfo($accessRtn['access_token'], $accessRtn['openid'])) {
+        echo "success";
 //        try {
 //            $db = new PDO('mysql:host=127.0.0.1;dbname=race', 'root', 'zxc');
 //            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -41,11 +37,13 @@ if (isset($_GET['code'])) {
 //            echo json_encode(['code' => '500', 'msg' => '服务器繁忙，请稍后重试']);
 //            die();
 //        }
-//    } else {
+    } else {
+        echo "fail1";
 //        echo json_encode(['code' => '401', 'msg' => '获取用户信息失败']);
-//    }
-//} else {
+    }
+} else {
+    echo "fail2";
 //    echo json_encode(['code' => '401', 'msg' => '获取token失败']);
-//}
+}
 
 ?>
