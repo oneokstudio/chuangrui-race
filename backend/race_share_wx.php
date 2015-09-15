d364613c81a77d43c1d79bc799a572f51f6373a8
sysumeepo <sysumeepo@gmail.com>
2015-09-15 14:17:37 +0800

    debug

--- backend/race_share_wx.php
+++ backend/race_share_wx.php
@@ -6,36 +6,36 @@
  * Time: 上午10:25
  */
 
-//require_once "lib/WxPay.Config.php";
-//require_once "lib/WxPay.JsApiPay.php";
-//require_once "lib/wechat.class.php";
-//require_once "lib/errCode.php";
-//
-//$opt = array(
-//    'token' => WxPayConfig::TOKEN,
-//    'appid' => WxPayConfig::APPID,
-//    'appsecret' => WxPayConfig::APPSECRET
-//);
-//
-////JSSDK签名
-//$we = new Wechat($opt);
-//$auth = $we->checkAuth();
-//$js_ticket = $we->getJsTicket();
-//if (!$js_ticket) {
-//    echo json_encode(['code' => '401', 'msg' => ErrCode::getErrText($we->errCode)]);
-//    exit;
-//}
-//$url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-//$js_sign = $we->getJsSign($url);
-//
-//echo json_encode($js_sign);
+require_once "lib/WxPay.Config.php";
+require_once "lib/WxPay.JsApiPay.php";
+require_once "lib/wechat.class.php";
+require_once "lib/errCode.php";
+
+$opt = array(
+    'token' => WxPayConfig::TOKEN,
+    'appid' => WxPayConfig::APPID,
+    'appsecret' => WxPayConfig::APPSECRET
+);
+
+//JSSDK签名
+$we = new Wechat($opt);
+$auth = $we->checkAuth();
+$js_ticket = $we->getJsTicket();
+if (!$js_ticket) {
+    echo json_encode(['code' => '401', 'msg' => ErrCode::getErrText($we->errCode)]);
+    exit;
+}
+$url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$js_sign = $we->getJsSign($url);
+
+echo json_encode($js_sign);
 
 
-require_once dirname(__FILE__) . '/vendor/autoload.php';
-
-$client = new Predis\Client();
-$client->set('foo', 'bar');
-$value = $client->get('foo');
-echo "value = $value";
+//require_once dirname(__FILE__) . '/vendor/autoload.php';
+//
+//$client = new Predis\Client();
+//$client->set('foo', 'bar');
+//$value = $client->get('foo');
+//echo "value = $value";
 
 ?>
