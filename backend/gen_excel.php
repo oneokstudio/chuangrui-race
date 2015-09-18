6454ceaafc9e20184b247bbe1a0222b7b52a4822
sysumeepo <sysumeepo@gmail.com>
2015-09-18 10:24:51 +0800

    mysql to excel script

---
+++ backend/gen_excel.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sysumeepo
+ * Date: 15/9/18
+ * Time: 上午10:03
+ */
+
+require_once './PhpExcel/PHPExcel.php';
+
+date_default_timezone_set('ETC/GMT-8');
+
+try {
+    $db = new PDO('mysql:host=127.0.0.1;dbname=race', 'root', 'zxc');
+    $result = $db->query('SELECT users.nickname, scores.score FROM users, scores WHERE users.openid = scores.openid ORDER BY scores.score DESC ');
+    $excel = new PHPExcel();
+    $excel->getActiveSheet()->setCellValue('A1', '昵称');
+    $excel->getActiveSheet()->setCellValue('B1', '分数');
+
+    $i = 2;
+    foreach ($result as $value) {
+        $excel->getActiveSheet()->setCellValue('A' . $i, $value['nickname']);
+        $excel->getActiveSheet()->setCellValue('B' . $i, $value['score']);
+        $i++;
+    }
+
+    $filename = '排名信息.xlsx';
+    header('Content-Type: application/vnd.ms-excel');
+    header('Content-Disposition: attachment;filename=$filename');
+    header('Cache-Control: max-age=0');
+    $objWriter = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
+    $objWriter->save($filename);
+
+    $db = null;
+} catch (PDOException $e) {
+    echo "can't connect to mysql";
+}
