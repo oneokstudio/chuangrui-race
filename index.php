840bf7571e14ed78ef05361eb14760851f6ab352
sysumeepo <sysumeepo@gmail.com>
2016-12-03 21:54:23 +0800

    debug

--- index.php
+++ index.php
@@ -7,19 +7,20 @@
  */
 
 
-require_once "backend/lib/WxPay.Config.php";
-require_once "backend/lib/wechat.class.php";
-
-
-$opt = array(
-    'appid' => WxPayConfig::APPID,    //填写高级调用功能的appid
-    'appsecret' => WxPayConfig::APPSECRET //填写高级调用功能的密钥
-);
-
-$we = new Wechat($opt);
-$redirectUrl = $we->getOauthRedirect("http://race.meedoo.cc/backend/callback.php");
+//require_once "backend/lib/WxPay.Config.php";
+//require_once "backend/lib/wechat.class.php";
+//
+//
+//$opt = array(
+//    'appid' => WxPayConfig::APPID,    //填写高级调用功能的appid
+//    'appsecret' => WxPayConfig::APPSECRET //填写高级调用功能的密钥
+//);
+//
+//$we = new Wechat($opt);
+//$redirectUrl = $we->getOauthRedirect("http://race.meedoo.cc/backend/callback.php");
 
 //echo $redirectUrl;
 
-header("Location:" . $redirectUrl);
+//header("Location:" . $redirectUrl);
+header('Location: http://race.meedoo.cc/release/html5/1443256816/?openid=0');
 ob_end_flush();
